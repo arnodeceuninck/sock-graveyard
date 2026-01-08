c917cc84e28eea48e4a7d390f6e71add468c1eda
Arno Deceuninck <arnodece@gmail.com>
2026-01-08 19:57:28 +0100

    Revert "Catch more errors"
    This reverts commit ce0e8f2b54ca681dd36a4cf4b54b3e9ec3b1974b.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,11 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/contexts/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 import ErrorBoundary from './src/components/ErrorBoundary';
-import { setupGlobalErrorHandlers } from './src/utils/errorReporting';
 
 export default function App() {
-  useEffect(() => {
-    // Setup global error handlers for uncaught errors
-    setupGlobalErrorHandlers();
-  }, []);
-
   return (
     <ErrorBoundary>
       <AuthProvider>
